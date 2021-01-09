c0e7c6b0ebaa91c28a1150c491ad27d7f17ac479
Ghazi Tozri <ghazi.tozri98@gmail.com>
2021-01-09 23:39:25 +0100

    Initial Commit

---
+++ iOSDiffableDataSource/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSDiffableDataSource
+//
+//  Created by Ghazi Tozri on 9/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
